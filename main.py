e457d46347402c1c90717829fa3bea9e015d58be
Ace <50378182+Fried-Squid@users.noreply.github.com>
2021-10-28 18:16:23 +0000

    set up header

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+##HEADER
+
+## IDEAS - BRIEF IS CLIMATE CHANGE
+# 
+#
+#
+
+
+## TODO LIST | STATUS
+#            |
+#            |
+#            |
+#            |
+
+## TEAM
+# Ace    -
+# Maxime -
+# Lucy   -
+
+##CODE
+
 import __hello__
-'test'
